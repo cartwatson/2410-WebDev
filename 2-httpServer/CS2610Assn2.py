18b2a88bcd8c2c3c850c020b284eea9bc263c972
cartwatson <cartwatson@gmail.com>
2022-09-28 19:48:53 -0600

    updated code, beautified

--- 2-httpServer/CS2610Assn2.py
+++ 2-httpServer/CS2610Assn2.py
@@ -5,18 +5,22 @@ import time
 
 
 class CS2610Assn2(BaseHTTPRequestHandler):
-    def dataHelper(self, type_location, fileName, requestHandler, code):
+    def dataHelper(self, requestHandler, type_location, fileName, code):
         header = f"Server: Carter's Server\nDate: {time.strftime('%c')}\nConnection: close\nCache-Control: max-age=10\n"
-        if code == "200 OK":
-            f = open(fileName, "rb")
-            data = f.read()
-            f.close()
+        if code == "200 OK": # regular request
+            if os.path.isfile(self.path[1:]):
+                f = open(fileName, "rb")
+                data = f.read()
+                f.close()
+            else: # debug page
+                data = fileName
             header += f"Content-type: {type_location}\n"
             header += f"Content-length: {len(data)}\n"
-        elif code != "301 Moved Permanently":
+        elif code != "301 Moved Permanently": # error
+            data = fileName
             header += f"Content-type: {type_location}\n"
             header += f"Content-length: {len(data)}\n"
-        else:
+        else: # redirect
             header += f"Location: {type_location}\n"
         header += "\n"
         requestHandler.wfile.write(bytes(f"HTTP/1.1 {code}\n" + header, encoding="utf-8"))
@@ -24,9 +28,9 @@ class CS2610Assn2(BaseHTTPRequestHandler):
             requestHandler.wfile.write(data)
 
     def do_GET(self):
-        # pages
+        # pages/images/icons
         if os.path.isfile(self.path[1:]):
-            self.dataHelper(mimetypes.guess_type(self.path[1:]), self.path[1:], self, "200 OK")
+            self.dataHelper(self, mimetypes.guess_type(self.path[1:]), self.path[1:], "200 OK")
         # 301 Redirects
         elif self.path in ["/", "/index", "/about", "/techtips+css", "/tips", "/techtips-css", "/help"] or self.path[0:4] == "/bio":
             if self.path in ["/", "/index"]:
@@ -37,7 +41,7 @@ class CS2610Assn2(BaseHTTPRequestHandler):
                 location = "techtips+css.html"
             elif self.path in ["/techtips-css", "/help"]:
                 location = "techtips-css.html"
-            self.dataHelper(location,  "", self, "301 Moved Permanently")
+            self.dataHelper(self, location,  "", "301 Moved Permanently")
         # errors
         elif self.path == "/debugging": # debug page
             # create page
@@ -53,45 +57,23 @@ class CS2610Assn2(BaseHTTPRequestHandler):
                 page += str(i) + "; "
             page += """</p>\n</body>\n</html>"""
             data = bytes(page, encoding="utf-8")
-            # create header
-            header = f"Server: Carter's Server\nDate: {time.strftime('%c')}\nConnection: close\nCache-Control: max-age=10\n"
-            header += f"Content-type: text/html\n"
-            header += f"Content-length: {len(data)}\n"
-            header += "\n"
-            # write
-            self.wfile.write(bytes("HTTP/1.1 200 OK\n" + header, encoding="utf-8"))
-            self.wfile.write(data)
+            self.dataHelper(self, "text/html", data, "200 OK")
         elif self.path == "/teapot": # ERROR 418
             page = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>418 teapot</title></head><body>
                 <h1>Error 418, I'm a teapot</h1><p>This page is short and stout.</p><a href="/">Go to main page</a></body></html>"""
             data = bytes(page, encoding="utf-8")
-            header = f"Server: Carter's Server\nDate: {time.strftime('%c')}\nConnection: close\nCache-Control: max-age=10\n"
-            header += f"Content-type: text/html\n"
-            header += f"Content-length: {len(data)}\n"
-            header += "\n"
-            self.wfile.write(bytes("HTTP/1.1 418 I'm a teapot\n" + header, encoding="utf-8"))
-            self.wfile.write(data)
+            self.dataHelper(self, "text/html", data, "418 I'm a teapot")
         elif self.path == "/forbidden": # ERROR 403
             page = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>403 Forbidden</title></head><body>
                 <h1>Error 403, Forbidden</h1><p>This is a stern warning that will make even the most determined hacker think twice about their chances.</p>
                 <p>A complaint has been filed to the FBI.</p><a href="/">Go to main page</a></body></html>"""
             data = bytes(page, encoding="utf-8")
-            header = f"Server: Carter's Server\nDate: {time.strftime('%c')}\nConnection: close\nCache-Control: max-age=10\n"
-            header += f"Content-type: text/html\n"
-            header += f"Content-length: {len(data)}\n"
-            header += "\n"
-            self.wfile.write(bytes("HTTP/1.1 403 Forbidden\n" + header, encoding="utf-8"))
-            self.wfile.write(data)
+            self.dataHelper(self, "text/html", data, "403 Forbidden")
         else: # ERROR 404
             page = """<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>404 Error</title></head>
                 <body><h1>Error 404, Page not found</h1><p>The requested page was not found</p><a href="/">Go to main page</a></body></html>"""
             data = bytes(page, encoding="utf-8")
-            header = f"Server: Carter's Server\nDate: {time.strftime('%c')}\nConnection: close\nCache-Control: max-age=10\n"
-            header += f"Content-type: text/html\n"
-            header += f"Content-length: {len(data)}\n"
-            header += "\n"
-            self.wfile.write(bytes("HTTP/1.1 404 Page Not Found\n" + header, encoding="utf-8"))
-            self.wfile.write(data)
+            self.dataHelper(self, "text/html", data, "404 Page Not Found")
 
 
 if __name__ == '__main__':
