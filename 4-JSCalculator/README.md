787c56929ff21635b80e09bd6ad14fc782be4147
cartwatson <cartwatson@gmail.com>
2022-10-28 14:41:02 -0600

    working condition, bug with deleting top div

--- 4-JSCalculator/README.md
+++ 4-JSCalculator/README.md
@@ -1,41 +1,12 @@
 # CS2610 Assignment 3 - Interactive JavaScript Calculator
 
-You are encouraged to replace the contents of this file with your own remarks
-or notes for the grader.
-
-Use this space to cite sources used to produce your submission.
-
-
-## Create a webpage using as little HTML as possible
-
-In order to give you better real-world experience with git, you will begin by
-taking up an incomplete project. Your first priority is to study this code to
-understand how it is organized, to learn what features are already provided,
-and find where to add the new code to satisfy the requirements set forth on
-Canvas. It is up to you to _read the code_ and understand what all of it is
-doing.
-
-Read the [assignment description on Canvas](https://usu.instructure.com/courses/566480/assignments/2858746)
-for complete assignment instructions.
-
-
-
-## Cloning this repository on your computer
-
-1.  Open a command window (Git+Bash on Windows)
-2.  Make sure that you are not already inside of a git repository by first
-    running `git status`.  You should see an error message like the following:
-    ```
-    fatal: Not a git repository (or any parent up to mount point /)
-    Stopping at filesystem boundary (GIT_DISCOVERY_ACROSS_FILESYSTEM not set).
-    ```
-3.  Clone this repository onto your own computer
-    ```
-    $ git clone git@gitlab.cs.usu.edu:erik.falor/cs2610-falor-erik-assn3.git
-    ```
-4.  Create a new repository of your own in GitLab.  Do *not* create a README.md
-    file from the web page
-5.  Follow the instructions provided by GitLab under the heading **Push an existing Git repository**
-
-Direct your questions about this process to the TA and the tutors who are
-well-prepared to assist you with this configuration.
+Sources
+* Title - https://stackoverflow.com/questions/413439/how-to-dynamically-change-a-web-pages-title
+* Date - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+* Date - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+* Number Input - https://www.w3schools.com/jsref/dom_obj_text.asp
+* Number Input - https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_text_create
+* Function/Adding New Div - https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+* Function parameters - https://www.w3schools.com/js/js_function_parameters.asp
+* Inner HTML - https://www.w3schools.com/js/js_htmldom_html.asp
+* 
--- 4-JSCalculator/script.js
+++ 4-JSCalculator/script.js
@@ -1,3 +1,143 @@
-/* TODO: Build up the HTML document by using JavaScript DOM manipulation functions.
- * Do not rely on changes you may have made to index.html because the grader won't use that file.
- */
+// replace title
+document.title = "JS Calculator";
+
+// Make div for calculator ---------------------------------------------------------------------------------------------
+const main = document.createElement('div');
+main.style.backgroundColor = "gray";
+main.classList.add('.stuff-box');
+
+// add descriptive text
+const p1 = document.createElement('p');
+const mainText = document.createTextNode("JS Calculator");
+p1.style.textAlign = "Center";
+p1.appendChild(mainText);
+
+const p2 = document.createElement('p');
+const descriptionText = document.createTextNode("Create an Expression");
+p2.style.textAlign = "Center";
+p2.appendChild(descriptionText);
+
+main.appendChild(p1);
+main.appendChild(p2);
+
+// operandOne  ---------------------------------------------------------------------------------------------
+const p3 = document.createElement('p');
+const operandOne = document.createElement("INPUT");
+operandOne.id = "operandOne";
+operandOne.setAttribute('type', 'number');
+operandOne.setAttribute('value', '12');
+p3.appendChild(operandOne);
+
+
+// operator ---------------------------------------------------------------------------------------------
+const op = document.createElement("select");
+op.id = "operator";
+const options = ["+", "-", "/", "*", "%", "**"];
+// input children
+for (var i = 0; i < options.length; i++) {
+    var option = document.createElement("option");
+    option.innerHTML = options[i];
+    op.appendChild(option);
+}
+p3.appendChild(op);
+
+// second operand ---------------------------------------------------------------------------------------------
+const operandTwo = document.createElement("INPUT");
+operandTwo.id = "operandTwo";
+operandTwo.setAttribute('type', 'number');
+operandTwo.setAttribute('value', '4');
+p3.appendChild(operandTwo);
+
+// input for color ---------------------------------------------------------------------------------------------
+const p4 = document.createElement('p')
+const colorInput = document.createElement("INPUT");
+colorInput.id = "colorInput"
+colorInput.setAttribute('type', 'color');
+p4.appendChild(colorInput);
+const colorText = document.createTextNode("Color Of New Result <Div>");
+p4.appendChild(colorText);
+p4.style.textAlign = "center";
+
+// create space for computational divs -----------------------------------------------------------------------
+const lowerDiv = document.createElement('div');
+
+// enter button ---------------------------------------------------------------------------------------------
+const enter = document.createElement('button');
+enter.innerHTML = "Compute";
+var lastDiv = null;
+enter.onclick = function() {result(operandOne.value, operandTwo.value, op.value, colorInput.value)};
+p3.appendChild(enter);
+p3.style.textAlign = "center";
+main.appendChild(p3);
+
+// add colorInput to main
+main.appendChild(p4);
+
+// add main/lowerDiv to document
+document.body.appendChild(main);
+document.body.appendChild(lowerDiv);
+
+
+// generate new div ---------------------------------------------------------------------------------------------
+function result(operandOne, operandTwo, op, color) {
+    const newDiv = document.createElement('div');
+    // add timestamp
+    const newSpan = document.createElement('span')
+    const event = new Date();
+    const options = { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric'};
+    const date = event.toLocaleDateString(undefined, options);
+    const dateNode = document.createTextNode(date + " -- ");
+    // TODO: fix time stamp styling
+    newSpan.appendChild(dateNode);
+    newSpan.classList.add('.timestamp');
+    newDiv.appendChild(newSpan);
+    
+    var content;
+    // handle error
+    if (operandOne === "" || operandTwo === "") {
+        newDiv.style.backgroundColor = "red";
+        content = document.createTextNode("Error! Missing Operand!");
+    } else {
+        // use selected color
+        newDiv.style.backgroundColor = color;
+        // compute result - parse operator
+        var result;
+        switch (op) {
+            case '+':
+                result = parseFloat(operandOne) + parseFloat(operandTwo);
+                break;
+            case '-':
+                result = parseFloat(operandOne) - parseFloat(operandTwo);
+                break;
+            case '/':
+                result = parseFloat(operandOne) / parseFloat(operandTwo);
+                break;
+            case '*':
+                result = parseFloat(operandOne) * parseFloat(operandTwo);
+                break;
+            case '%':
+                result = parseFloat(operandOne) % parseFloat(operandTwo);
+                break;
+            case '**':
+                result = parseFloat(operandOne) ** parseFloat(operandTwo);
+                break;
+            default:
+                result = 0;
+                console.log("error");
+          }
+        // print result
+        content = document.createTextNode(operandOne + " " + op + " " + operandTwo + " = " + result);
+    }
+    newDiv.appendChild(content);
+    newDiv.style.textAlign = "Center";
+
+    // delete onclick
+    newDiv.onclick = function() {
+        this.remove();
+    };
+
+
+    // above existing results
+    lowerDiv.insertBefore(newDiv, lastDiv);
+    lastDiv = newDiv;    
+}
