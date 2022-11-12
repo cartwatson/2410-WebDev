9801f42b689e91ae3976a9c590f2025c20ad87c5
cartwatson <cartwatson@gmail.com>
2022-11-11 22:46:21 -0700

    updating

--- 5-WYWIG/unitconv/views.py
+++ 5-WYWIG/unitconv/views.py
@@ -1,28 +1,45 @@
+from django.http import JsonResponse, QueryDict
 from django.shortcuts import render
 
-# Create your views here.
-def convert(request):
-# TODO: responds to requests of the form
-    # convert?from=lb&to=t_oz&value=3.14159
-        # from is a string which identifies the type of units the input value represents
-        # value is a floating-point number representing the number of from units
-        # to is a string representing the type of units the user wants value converted into
-    # result of a valid request is JSON
-        # value is a floating-point number representing the number of units
-        # units identify the units of the output value, which must match the to parameter of the request
-    # result of an invalid query
-        # a JSON object indicating an error message is returned instead
-    # makes its calculations through django model
-        # sensible schema, one table
-        # algorithm for converting units
-        # create data migration to initialize db w/ necessary converstion factors
-            # this is the only place to hard code factors
-            # US Ton (T)
-            # Gram (g)
-            # Troy Ounce (t_oz)
-            # kilogram (kg)
-            # imperial pound (lb)
-            # ounce (oz)
-        # must be able to convert between all unit types
-    
+def helper():
     pass
+
+# Create your views here.
+# TODO: responds to requests of the form 'convert?from=lb&to=t_oz&value=3.14159'
+def convert(request):
+    context = {}
+    # from is a string which identifies the type of units the input value represents
+    fromType = request.GET['from']
+    # to is a string representing the type of units the user wants value converted into
+    toType = request.GET['to']
+    # value is a floating-point number representing the number of from units
+    value = request.GET['value']
+
+    # error checking values
+    fromTypeValid = False
+    toTypeValid = False
+    valueTypeValid = False
+    # check unit types
+    unitTypes = ['T', 'g', 't_oz', 'kg', 'lb', 'oz']
+    for unit in unitTypes:
+        if fromType == unit:
+            fromTypeValid = True
+        if toType == unit:
+            toTypeValid = True
+    # check for valid float
+    try:
+        float(value)
+        valueTypeValid = True
+    except ValueError:
+        valueTypeValid = False
+    # appropriate response
+    if not fromTypeValid or not toTypeValid or not valueTypeValid:
+        # responses were not valid types
+        context['error'] = 'Invalid unit conversion request'
+    else:
+        context['units'] = toType
+        context['value'] = 
+
+
+    # must be able to convert between all unit types
+    return JsonResponse(context)
